8bf8a0bf152bbedac3b8a7629d1689746b77af61
gines <gines.collado@gmail.com>
2020-08-13 18:34:44 +0200

    cambio

--- server/config/config.js
+++ server/config/config.js
@@ -6,7 +6,7 @@ process.env.PORT = process.env.PORT || 3000;
 // =================
 // Entorno
 // =================
-process.env.NODE_ENV = process.env.NODE_ENV || "heroku";
+process.env.NODE_ENV = process.env.NODE_ENV || "dev";
 
 // =================
 // Base de datos
